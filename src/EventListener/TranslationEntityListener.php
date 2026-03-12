cbab2d79ac4bcb9185798db328b80301b55fa7c0
lordjancso <lordjancso@gmail.com>
2026-03-12 09:37:33 +0100

    Change global event listener to entity listener for better performance

--- src/EventListener/TranslationEntityListener.php
+++ src/EventListener/TranslationEntityListener.php
@@ -2,33 +2,19 @@
 
 namespace Lordjancso\TranslationBundle\EventListener;
 
-use Doctrine\ORM\Event\PrePersistEventArgs;
-use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Lordjancso\TranslationBundle\Entity\TranslationKey;
 use Lordjancso\TranslationBundle\Entity\TranslationValue;
 
 class TranslationEntityListener
 {
-    public function prePersist(PrePersistEventArgs $eventArgs): void
+    public function prePersist(TranslationKey|TranslationValue $entity): void
     {
-        $entity = $eventArgs->getObject();
-
-        if (!$entity instanceof TranslationKey && !$entity instanceof TranslationValue) {
-            return;
-        }
-
         $entity->setCreatedAt(new \DateTime());
         $entity->setUpdatedAt(new \DateTime());
     }
 
-    public function preUpdate(PreUpdateEventArgs $eventArgs): void
+    public function preUpdate(TranslationKey|TranslationValue $entity): void
     {
-        $entity = $eventArgs->getObject();
-
-        if (!$entity instanceof TranslationKey && !$entity instanceof TranslationValue) {
-            return;
-        }
-
         $entity->setUpdatedAt(new \DateTime());
     }
 }
